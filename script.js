9b1b2803f4425785fa0db69b01e4b0f469e3d9c4
Grace Hartzell <gracehartzell@utexas.edu>
2018-08-07 18:20:36 -0500

    Completed red and black checkerboard and refactored (yes, I forgot to commit between refactors.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Your JS goes here
+document.addEventListener("DOMContentLoaded",function() {
+    
+    for (let i = 0; i < 81; i++) {
+        const squares = document.createElement('div');
+        squares.setAttribute('class','squarePattern')
+        document.body.appendChild(squares); 
+        squares.style.width = '11.1%';
+        squares.style.paddingBottom = '11.1%';
+        squares.style.cssFloat = 'left';
+        
+        (i % 2 ) ? squares.style.backgroundColor = "red" 
+        : squares.style.backgroundColor = "darkslategrey";
+    }
+});
+
